40f825a5d6341e56f7120f79d9c0727271bf471a
m3nf1s <titan94@mail.ru>
2025-07-11 00:12:39 +0300

    Setting Up SDL & Creating First Window

--- SDL_Game.cpp
+++ SDL_Game.cpp
@@ -1,8 +1,31 @@
 ﻿#include <iostream>
 
+#include <SDL3/SDL.h>
 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+    
+    SDL_Window* window = SDL_CreateWindow("SDL_Game", 800, 600, SDL_WINDOW_RESIZABLE);
+    if (!window)
+    {
+        return 1;
+    }
+    
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer)
+    {
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(5000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
